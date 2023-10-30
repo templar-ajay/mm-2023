203c4eefee38aee95b6e2c8c345330a8d9c27a31
templar-ajay <templar.ajay@gmail.com>
2023-10-30 17:27:12 +0530

    ; removed

--- pages/_document.js
+++ pages/_document.js
@@ -38,7 +38,7 @@ export default function Document() {
             });`
           }}
         />
-        ;
+
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700&family=Open+Sans:wght@300;400;500;600;700;800&display=swap"
           rel="stylesheet"
