075ac0b9abffd29afc1df5511c7bf3d11f981786
templar-ajay <templar.ajay@gmail.com>
2023-11-09 13:27:26 +0530

    GTM

--- pages/_document.js
+++ pages/_document.js
@@ -93,7 +93,7 @@ export default function Document() {
             src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
             height="0"
             width="0"
-            style="display:none;visibility:hidden"
+            style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
 
