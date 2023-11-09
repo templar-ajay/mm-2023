f21e8c0a70d03cc2183cc4986373f6b7611b950f
templar-ajay <templar.ajay@gmail.com>
2023-11-09 13:26:10 +0530

    GTM

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Script from "next/script";
 
 export default function Document() {
-  const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || "GTM-P3D87XN";
+  const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || "GTM-WRL5GCRR";
 
   return (
     <Html lang="en">
@@ -85,12 +85,12 @@ export default function Document() {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WRL5GCRR');`
+            })(window,document,'script','dataLayer','${GTM_ID}');`
           }}
         />
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-WRL5GCRR"
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
             height="0"
             width="0"
             style="display:none;visibility:hidden"
