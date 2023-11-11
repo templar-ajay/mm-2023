0bf9ffbf02345841067e19f32903e493dd340c91
templar-ajay <templar.ajay@gmail.com>
2023-11-11 06:26:22 +0530

    dropdown

--- components/common/Dropdown.js
+++ components/common/Dropdown.js
@@ -29,7 +29,7 @@ export default function Dropdown({ name, menuItems }) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-1 w-40 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 z-10 mt-1 w-full origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
             {menuItems.map((item) => (
               <Menu.Item>
@@ -40,7 +40,7 @@ export default function Dropdown({ name, menuItems }) {
                     href={item.path}
                     className={classNames(
                       active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                      "block px-4 py-2 text-sm text-center"
+                      "block py-2 text-sm text-center"
                     )}
                   >
                     {item.title}
