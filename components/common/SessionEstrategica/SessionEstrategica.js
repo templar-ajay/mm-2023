4724c7d53c2872cdd6b4241ac8a7f523eda083a8
templar-ajay <templar.ajay@gmail.com>
2023-11-20 10:21:20 +0530

    updating the book a session script

--- components/common/SessionEstrategica/SessionEstrategica.js
+++ components/common/SessionEstrategica/SessionEstrategica.js
@@ -10,12 +10,20 @@ export default function SessionEstrategica(data) {
       <SectionWrapper pb={150} pbs={105}>
         <SessionEstrategicaStyles>
           <div className="container container__tight">
-            <iframe
+            {/* <iframe
               src="https://api.leadconnectorhq.com/widget/survey/OdKHQGaAzS9V553Lo5Zt"
               title="Sesión Estratégica de 30 Minutos con Medical Marketing"
-            />
+            /> */}
+            <iframe
+              src="https://api.leadconnectorhq.com/widget/survey/zz0n1CnJ3qifljz6v19G"
+              style="border:none;width:100%;"
+              scrolling="no"
+              id="zz0n1CnJ3qifljz6v19G"
+              title="Muhammad - This is the One to Modify"
+            ></iframe>
             <Helmet>
-              <script src="https://api.leadconnectorhq.com/js/form_embed.js"></script>
+              {/* <script src="https://api.leadconnectorhq.com/js/form_embed.js"></script> */}
+              <script src="https://link.msgsndr.com/js/form_embed.js"></script>
             </Helmet>
           </div>
         </SessionEstrategicaStyles>
